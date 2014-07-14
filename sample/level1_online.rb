cf3f1ae3aa49a8ef533d0f69e91ede669ff4a201
Timur Adigamov <tadigamov@klick.com>
2014-07-14 08:21:52 -0400

    added level1 sample

--- sample/level1_online.rb
+++ sample/level1_online.rb
@@ -1,8 +1,8 @@
-require '../lib/history_client'
+require '../lib/level1_client'
 
 c = IQ::Level1Client.new
 c.open
-c.add("MSFT")
+c.add("@EU#")
 c.process_request(1) do |line|
 	puts line.to_s
 end
