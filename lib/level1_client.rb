af62617f63f0f8e80f6f1c06d045dfa6d8fc0505
Timur Adigamov <tadigamov@klick.com>
2014-07-14 08:21:28 -0400

    added online level 1 client

--- lib/level1_client.rb
+++ lib/level1_client.rb
@@ -1,12 +1,48 @@
 require 'socket'
 
 module IQ
+	class Level1Tick
+		attr_accessor :type, :symbol, :last, :change, :percent_change, :total_volume, :incremental_volume, :high, :low, :bid, :ask, :bid_size, :ask_size, :tick, :bid_tick
+		def self.parse(line)
+			fields = line.split(',')
+			tick = Level1Tick.new
+			tick.type = fields[17]
+			tick.bid = fields[10]
+			tick.bid_size = fields[12]
+			tick.ask = fields[11]
+			tick.ask_size = fields[13]
+			tick.tick = fields[14]
+			tick.last = fields[3]
+			tick.incremental_volume = fields[7]
+			tick
+		end
+
+		def to_s
+			if (@type =~ /b/)
+				"Bid: #{@bid_size}@#{@bid}"
+			elsif (@type =~ /a/)
+				"Ask: #{@ask_size}@#{@ask}"
+			elsif (@type =~ /t/)
+				"Trade: #{@last} #{@incremental_volume}"
+			elsif (@type =~ /T/)
+				"extendedTrade"
+			elsif !@type.nil?
+				"other"
+			else
+				nil
+			end			
+		end
+	end
+
 	class Level1Client
 		def initialize(options = {})
 			parse_options(options)			
 		end
 
 		def parse_options(options)
+			@host = options[:host] || 'localhost'
+			@port = options[:port] || 5009
+			@name = options[:name] || 'DEMO'
 		end
 
 		def open
@@ -33,8 +69,7 @@ module IQ
 				elsif line =~ /!ENDMSG!,/
 					break
 				end
-				#yield parse.call(line)
-				yield parse.call(line)
+				yield Level1Tick.parse(line)				
 			end
 			if exception
 				raise exception
