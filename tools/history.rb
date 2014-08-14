9a668955a15974363596ae53c4fb1fb501ecc895
Timur Adigamov <tadigamov@klick.com>
2014-08-13 21:07:39 -0400

    new functionality for symbol list loading

--- tools/history.rb
+++ tools/history.rb
@@ -4,10 +4,26 @@ require 'pp'
 require 'date'
 require '../lib/history_client'
 
-def client_proxy(iq_client, options, &block)
-  iq_client.send(options[:method], options) do |line|
-    block.call line
-  end
+MONTHS=['F','G','H','J','K','M','N','Q','U','V','X','Z']
+
+class FileObserver
+	def initialize(file)
+		@file = file
+	end
+	def update(tick)
+		@file.puts tick.to_csv		
+	end
+end
+
+def client_proxy(iq_client, options, o)
+  iq_client.send(options[:method], options, o)
+end
+
+def get_valid_codes(today, months)
+  current_month = today.month
+  valid_months = months.map{|m| MONTHS.index(m) + 1}.select{|m| m >= current_month}.map{|m| "#{MONTHS[m-1]}#{today.year-2000}"}
+  valid_months = valid_months + months.slice(0, 2 - valid_months.length).map{|m| m + "#{today.year-2000+1}"} if valid_months.length < 2    
+  valid_months
 end
 
 options = {}
@@ -20,17 +36,19 @@ opts = OptionParser.new do |opts|
   end
 
   opts.on("-f", "--from DATE", "Start date for history request") do |from|
-    d = DateTime.parse(from)
-    options[:from] = Time.new(d.year, d.month, d.day, d.hour, d.min, d.sec)
+    options[:from] = DateTime.parse(from) #Time.new(d.year, d.month, d.day, 0, 0, 0)
   end
 
   opts.on("-t", "--to DATE", "End date for history request") do |to|
-    d = DateTime.parse(to)
-    options[:to] = Time.new(d.year, d.month, d.day, d.hour, d.min, d.sec)
+    options[:to] = DateTime.parse(to) #Time.new(d.year, d.month, d.day, 23, 59, 59)
   end
 
   opts.on("-o", "--output FILE", "Output file for history data") do |output|
     options[:output] = output
+  end
+
+  opts.on("-i", "--input FILE", "Input file with list of future contracts") do |input|
+    options[:input] = input
   end
 
   opts.on("-t", "--type [TYPE]", [:tick, :ohlc, :dwm], "Select data type (tick, ohlc, dwm)") do |type|    
@@ -50,9 +68,10 @@ end
 
 begin
   opts.parse!
-  mandatory = [:symbol, :from, :to]                                         
+  mandatory = [:from, :to]                                         
   missing = mandatory.select{ |param| options[param].nil? }
-  if not missing.empty?                                            
+#TODO fix
+  if not missing.empty? || (options[:input].nil? && options[:symbol].nil?)
     puts "Missing options: #{missing.join(', ')}"                  
     puts opts
     exit                                                           
@@ -64,17 +83,45 @@ rescue OptionParser::InvalidOption, OptionParser::MissingArgument
 end 
 
 options[:method] ||= :get_tick_range 
-# create default output file name
-options[:output] ||= options[:symbol].gsub(/[@\$\^#]/,'') + '.csv'
-# default duration for ohlc is 5m
 options[:duration] ||= 300 if options[:type] == :ohlc
 
-output_file = File.new(options[:output], "w")
+contracts = {}
+if options[:input].nil?  
+	contracts[options[:symbol]] = []
+else
+	contracts_file = File.open(options[:input])
+	contracts_file.each do |line|
+		f = line.split(';')
+		contracts[f[0]] = f[3].split(',')
+	end
+end
+
 iq_client = IQ::HistoryClient.new
 iq_client.open
 
-client_proxy(iq_client, options) do |tick|
-	output_file.puts tick.to_csv
-end
-output_file.close
+from_date = options[:from]
+while from_date <= options[:to]  
+  # TODO: check if day was holiday
+  contracts.keys.each do |contract|
+    # no need to build contract name
+    codes = []
+    if contracts[contract].nil?
+      codes = [contract]      
+    else
+      codes = get_valid_codes(from_date, contracts[contract]).map{|c| "#{contract}#{c}"}      
+    end
+    codes.each do |code|
+      output_file = File.new(code.gsub("@","")+"#{from_date.year}#{from_date.month}#{from_date.day}", "w")
+      options[:symbol] = code
+      options[:from] = Time.new(from_date.year, from_date.month, from_date.day, 0, 0, 0)
+      options[:to] = Time.new(from_date.year, from_date.month, from_date.day, 23, 59, 59)
+      o = FileObserver.new(output_file)
+      client_proxy(iq_client, options, o) 
+      iq_client.run
+    end
+  end  
 
+  from_date = from_date.next
+end
+
+# options[:output] ||= options[:symbol].gsub(/[@\$\^#]/,'') + '.csv'
