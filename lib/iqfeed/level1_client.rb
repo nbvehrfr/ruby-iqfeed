9b945564a67364520ae11af3b8ddaefc4ae779d9
Tim <dev@adigamov.ca>
2014-11-15 00:13:15 +0000

    sample and tools moved to lib

--- lib/iqfeed/level1_client.rb
+++ lib/iqfeed/level1_client.rb
@@ -3,39 +3,51 @@ require 'observer'
 
 module Iqfeed
 	class Level1Observer
-		attr_accessor :symbol
+		attr_accessor :symbol, :bid, :ask
 
 		def initialize(symbol)
 			@symbol = symbol
 		end
 
 		def update(tick)
-			puts tick.to_s
+			@bid = tick.bid if @bid.nil? && tick.type =~ /(t|b)/
+			@bid = tick.bid if tick.type =~ /b/
+			@ask = tick.ask if @ask.nil? && tick.type =~ /(t|a)/
+			@ask = tick.ask if tick.type =~ /a/
+			puts tick.to_s(@bid, @ask) if tick.sym == @symbol && tick.type =~ /t/
 		end
 	end
 
 	class Level1Tick
-		attr_accessor :type, :time_stamp, :last_price, :total_volume, :last_size, :bid, :ask
+		attr_accessor :type, :time_stamp, :last_price, :total_volume, :last_size, :bid, :ask, :sym
 		def self.parse(line)
 			fields = line.split(',')
 			tick = Level1Tick.new
 			tick.type = fields[17]
+			tick.sym = fields[1]
 			tick.bid = fields[10]
 			tick.ask = fields[11]
 			tick.last_price = fields[3]
 			tick.last_size = fields[7]
 			tick.total_volume = fields[6]
 			tick.time_stamp = fields[65]
-			tick.type =~ /t/ ? tick : nil
+			tick.type =~ /(t|a|b)/ ? tick : nil
 		end
 
-		def to_s
-#			if (@type =~ /b/)
-#				"Bid: #{@bid_size}@#{@bid}"
-#			elsif (@type =~ /a/)
-#				"Ask: #{@ask_size}@#{@ask}"
+		def to_s(bid, ask)
 			if (@type =~ /t/)
-				"Timestamp:#{@time_stamp} LastPrice:#{@last_price} LastSize:#{@last_size} TotalVolume:#{@total_volume} Bid:#{@bid} Ask:#{@ask}"			
+				if @last_price == @bid
+					operation = 'SELL'
+				elsif @last_price == @ask
+					operation = 'BUY'
+				elsif @last_price == bid
+					operation = 'SELL'
+				elsif @last_price == ask
+					operation = 'BUY'
+				else
+					operation = 'UNKNOWN'
+				end
+				"Symbol: #{@sym} TS:#{@time_stamp} Size:#{@last_size} Price:#{@last_price} Bid:#{@bid} Ask:#{@ask} #{operation}"
 #			elsif (@type =~ /T/)
 #				"extendedTrade"
 #			elsif !@type.nil?
